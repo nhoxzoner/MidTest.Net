8adf3ddc87f59631842e601ed945b6b862c2da7b
hhkhang0911@gmail.com <hhkhang0911@gmail.com>
2022-12-07 08:50:59 +0700

    Update connection

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using MidTest.Net.Data;
 using MidTest.Net.Models;
 using System;
 using System.Collections.Generic;
@@ -12,14 +13,18 @@ namespace MidTest.Net.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly QuanLyTranDauDbContext _context;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, QuanLyTranDauDbContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
         public IActionResult Index()
         {
+            var dsViTri = _context.ViTri.ToList();
+            ViewBag["ViTri"] = dsViTri;
             return View();
         }
 
--- Startup.cs
+++ Startup.cs
@@ -1,9 +1,11 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using MidTest.Net.Data;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -23,6 +25,11 @@ namespace MidTest.Net
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<QuanLyTranDauDbContext>(options =>
+            {
+                options.UseSqlServer("Server=.\\SQLEXPRESS;Database=QuanLyTranDau;Integrated Security=True;");
+            });
+
             services.AddControllersWithViews();
         }
 
